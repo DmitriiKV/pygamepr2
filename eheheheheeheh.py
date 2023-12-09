dad83f2d7e20ce8545942d513763a1cf6f925bc2
Dmitrii <dm1trymkv@yandex.ru>
2023-12-09 10:31:43 +0300

    2 (sprites one)

--- eheheheheeheh.py
+++ eheheheheeheh.py
@@ -1,11 +1,12 @@
 import os
+import random
 import sys
 import pygame
 
 pygame.init()
 size = width, height = 500, 500
 screen = pygame.display.set_mode(size)
-image = pygame.Surface([100, 100])
+screen.fill(pygame.Color('white'))
 
 
 def load_image(name, colorkey=None):
@@ -24,18 +25,36 @@ def load_image(name, colorkey=None):
     return image
 
 
-image = load_image("jorik.jpg")
-img_1 = pygame.transform.scale(image, (200, 100))
-img_2 = pygame.transform.scale(image, (100, 200))
+# image = load_image("jorik.jpg")
+# img_1 = pygame.transform.scale(image, (200, 100))
+# img_2 = pygame.transform.scale(image, (100, 200))
+
+all_sprites = pygame.sprite.Group()
+# sprite = pygame.sprite.Sprite()
+# sprite.image = load_image('bob.png')
+# sprite.rect = sprite.image.get_rect()
+# all_sprites.add(sprite)
+# sprite.rect.x = 5
+# sprite.rect.y = 50
+
+bob_image = load_image('bob.png')
+for i in range(50):
+    bob = pygame.sprite.Sprite(all_sprites)
+    bob.image = bob_image
+    bob.rect = bob.image.get_rect()
+
+    bob.rect.x = random.randrange(width)
+    bob.rect.y = random.randrange(height)
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    screen.blit(image, (10, 10))
-    screen.blit(img_1, (150, 150))
-    screen.blit(img_2, (250, 250))
+    # screen.blit(image, (10, 10))
+    # screen.blit(img_1, (150, 150))
+    # screen.blit(img_2, (250, 250))
+    all_sprites.draw(screen)
 
     pygame.display.flip()
 pygame.quit()
